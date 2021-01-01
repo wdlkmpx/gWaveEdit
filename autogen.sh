1394393dc61d070695a53d465480ab1442f80985
wdlkmpx <wdlkmpx@gmail.com>
2021-01-01 22:47:06 +0800

    update autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -9,21 +9,36 @@ test -z "$AUTOMAKE"   && AUTOMAKE=automake
 test -z "$ACLOCAL"    && ACLOCAL=aclocal
 test -z "$AUTOCONF"   && AUTOCONF=autoconf
 test -z "$AUTOHEADER" && AUTOHEADER=autoheader
+test -z "$LIBTOOLIZE" && LIBTOOLIZE=$(which libtoolize glibtoolize | head -1)
 
-set -x 
+if test "$1" == "verbose" || test "$1" == "--verbose" ; then
+	set -x
+	verbose='--verbose'
+	verbose2='--debug'
+fi
 
 #Get all required m4 macros required for configure
-libtoolize --copy --force || exit 1
-$ACLOCAL -I m4 || exit 1
+$LIBTOOLIZE ${verbose} --copy --force || exit 1
+$ACLOCAL ${verbose} -I m4 || exit 1
 
 #Generate config.h.in
-$AUTOHEADER --force || exit 1
+$AUTOHEADER ${verbose} --force || exit 1
 
 #Generate Makefile.in's
 touch config.rpath
-$AUTOMAKE --add-missing --copy --force || exit 1
-AUTOMAKE=$AUTOMAKE intltoolize -c --automake --force || exit 1
+$AUTOMAKE ${verbose} --add-missing --copy --force || exit 1
+
+if grep "IT_PROG_INTLTOOL" configure.ac >/dev/null ; then
+	intltoolize ${verbose2} -c --automake --force || exit 1
+	# po/Makefile.in.in has these lines:
+	#    mostlyclean:
+	#       rm -f *.pox $(GETTEXT_PACKAGE).pot *.old.po cat-id-tbl.tmp
+	# prevent $(GETTEXT_PACKAGE).pot from being deleted by `make clean`
+	sed 's/pox \$(GETTEXT_PACKAGE).pot/pox/' po/Makefile.in.in > po/Makefile.in.inx
+	mv -f po/Makefile.in.inx po/Makefile.in.in
+fi
 
 #generate configure
-$AUTOCONF --force || exit 1
+$AUTOCONF ${verbose} --force || exit 1
 
+rm -rf autom4te.cache
