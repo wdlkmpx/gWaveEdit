66b7e76ed3b113ed53d23c0a8004b9da7384083b
wdlkmpx <wdlkmpx@gmail.com>
2021-01-04 11:05:10 +0800

    rawdialog.c: use GtkDialog

--- src/rawdialog.c
+++ src/rawdialog.c
@@ -36,26 +36,27 @@ static FormatSelector *fs;
 static Intbox *offset_box;
 static gint maxhdrsize;
 
-static void rawdialog_ok(GtkButton *button, gpointer user_data)
+static void
+rawdialog_response (GtkDialog * dlg, int response, gpointer user_data)
 {
-     if (format_selector_check(fs) || 
-	 intbox_check_limit(offset_box,0,maxhdrsize,_("header size"))) {
-          g_signal_stop_emission_by_name(G_OBJECT(button),"clicked");
-          return;
-     }
-     format_selector_get(fs,&fmt);
-     ok_flag = TRUE;
-}
-
-static void rawdialog_destroy(GtkObject *object, gpointer user_data)
-{
-     destroy_flag = TRUE;
+   if (response == GTK_RESPONSE_OK)
+   {
+      if (format_selector_check(fs) || intbox_check_limit(offset_box,0,maxhdrsize,_("header size")))
+      {
+         g_signal_stop_emission_by_name (dlg, "response");
+         return;
+      }
+      format_selector_get (fs,&fmt);
+      ok_flag = TRUE;
+   }
+   gtk_widget_destroy (GTK_WIDGET (dlg));
+   destroy_flag = TRUE;
 }
 
 Dataformat *rawdialog_execute(gchar *filename, gint filesize, guint *offset)
 {
-     GtkWindow *w;
-     GtkWidget *a,*b,*c;
+     GtkWidget * dialog, * vbox;
+     GtkWidget *b,*c;
      gchar *ch;
      
      memset(&fmt,0,sizeof(fmt));
@@ -69,51 +70,48 @@ Dataformat *rawdialog_execute(gchar *filename, gint filesize, guint *offset)
      dataformat_get_from_inifile("rawDialog",TRUE,&fmt);    
      maxhdrsize = filesize;
      
-     w = GTK_WINDOW(gtk_window_new(GTK_WINDOW_TOPLEVEL));
-     gtk_window_set_title(w,_("Unknown file format"));
-     gtk_window_set_modal(w,TRUE);
-     gtk_container_set_border_width(GTK_CONTAINER(w),5);
-     g_signal_connect(G_OBJECT(w),"destroy",
-          G_CALLBACK(rawdialog_destroy),NULL);
-     a = gtk_vbox_new(FALSE,3);
-     gtk_container_add(GTK_CONTAINER(w),a);
+     //w = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+     dialog = gtk_dialog_new ();
+     gtk_window_set_title (GTK_WINDOW (dialog), _("Unknown file format"));
+     gtk_window_set_modal (GTK_WINDOW (dialog), TRUE);
+     gtk_window_set_resizable (GTK_WINDOW (dialog), FALSE);
+     gtk_container_set_border_width (GTK_CONTAINER (dialog),5);
+
+     vbox = gtk_dialog_get_content_area (GTK_DIALOG (dialog));
+     gtk_box_set_spacing (GTK_BOX (vbox), 3);
+
      ch = g_strdup_printf(_("The format of file '%s' could not be recognized.\n\n"
 			  "Please specify the sample format below."),filename);
      b = gtk_label_new(ch);
      gtk_label_set_line_wrap(GTK_LABEL(b),TRUE);
-     gtk_box_pack_start(GTK_BOX(a),b,TRUE,FALSE,0);
+     gtk_box_pack_start (GTK_BOX (vbox), b, TRUE, FALSE, 0);
      g_free(ch);
      b = format_selector_new(TRUE);
      fs = FORMAT_SELECTOR(b);
      format_selector_set(fs,&fmt);
-     gtk_container_add(GTK_CONTAINER(a),b);
+     gtk_container_add (GTK_CONTAINER (vbox), b);
      b = gtk_hbox_new(FALSE,0);
-     gtk_container_add(GTK_CONTAINER(a),b);
+     gtk_container_add (GTK_CONTAINER (vbox), b);
      c = gtk_label_new(_("Header bytes: "));
      gtk_box_pack_start(GTK_BOX(b),c,FALSE,FALSE,0);
      c = intbox_new(inifile_get_guint32("rawDialog_offset",0));
      offset_box = INTBOX(c);
      gtk_box_pack_start(GTK_BOX(b),c,FALSE,FALSE,0);
      b = gtk_hseparator_new();
-     gtk_container_add(GTK_CONTAINER(a),b);
-     b = gtk_hbutton_box_new();
-     gtk_button_box_set_layout(GTK_BUTTON_BOX(b),GTK_BUTTONBOX_END);
-     gtk_container_add(GTK_CONTAINER(a),b);
-     c = gtk_button_new_with_label(_("OK"));
-     g_signal_connect(G_OBJECT(c),"clicked",G_CALLBACK(rawdialog_ok),
-          NULL);
-     g_signal_connect_swapped(G_OBJECT(c),"clicked",
-          G_CALLBACK(gtk_widget_destroy), w);
-     gtk_container_add(GTK_CONTAINER(b),c);
-     c = gtk_button_new_with_label(_("Cancel"));
-     g_signal_connect_swapped(G_OBJECT(c),"clicked",
-          G_CALLBACK(gtk_widget_destroy), w);
-     gtk_container_add(GTK_CONTAINER(b),c);
+     gtk_container_add (GTK_CONTAINER (vbox), b);
+
+     gtk_dialog_add_button (GTK_DIALOG (dialog), _("_OK"), GTK_RESPONSE_OK);
+     gtk_dialog_add_button (GTK_DIALOG (dialog), _("_Cancel"), GTK_RESPONSE_CANCEL);
+     g_signal_connect (dialog, "response", G_CALLBACK (rawdialog_response), NULL);
      
      ok_flag = destroy_flag = FALSE;
      
-     gtk_widget_show_all(GTK_WIDGET(w));
-     while (!destroy_flag) mainloop();
+     gtk_widget_show_all (dialog);
+
+     // wait for the dialog to close
+     while (!destroy_flag) {
+        mainloop();
+     }
      
      if (!ok_flag) return NULL;
 
