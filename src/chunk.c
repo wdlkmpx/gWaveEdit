wdlkmpx/gWaveEdit

commit a8a2a668989918cd2b204c2217529bfcd83237cd

remove src/gettext.h

src/chunk.c
@@ -20,15 +20,12 @@
  */
 
 
-#include <config.h>
-
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
 #include <errno.h>
 #include <math.h>
 #include <sys/stat.h>
-#include <gtk/gtk.h>
 #include "gtkfiles.h"
 #include "um.h"
 #include "chunk.h"
@@ -38,7 +35,6 @@
 #include "rawdialog.h"
 #include "tempfile.h"
 #include "rateconv.h"
-#include "gettext.h"
 
 G_DEFINE_TYPE(Chunk,chunk,GTK_TYPE_OBJECT)
 

src/chunkview.c
@@ -19,10 +19,6 @@
  */
 
 
-#include <config.h>
-
-
-
 #include <math.h>
 #include "chunkview.h"
 #include "main.h"

src/datasource.c
@@ -32,7 +32,6 @@
 
 #include "datasource.h"
 #include "tempfile.h"
-#include "gettext.h"
 
 G_DEFINE_TYPE(Datasource,datasource,GTK_TYPE_OBJECT)
 

src/document.c
@@ -25,7 +25,6 @@
 #include "document.h"
 #include "filetypes.h"
 #include "player.h"
-#include "gettext.h"
 
 G_DEFINE_TYPE(Document,document,GTK_TYPE_OBJECT)
 

src/main.h
@@ -24,13 +24,28 @@
 #ifndef MAIN_H_INCLUDED
 #define MAIN_H_INCLUDED
 
+#include <config.h>
 #include <stdio.h>
-#include <gtk/gtk.h>
-#include "gtkcompat.h"
 #include <sys/time.h>
 #include <sys/types.h>
 #include <time.h>
 #include <string.h>
+#include "gtkcompat.h"
+
+#ifdef ENABLE_NLS
+#include <libintl.h>
+#include <locale.h>
+#define _(x) gettext(x)
+#define N_(x) (x)
+#else
+#define _(x) (x)
+#define N_(x) (x)
+#define setlocale(cat,locale)
+#define bindtextdomain(Domainname,Dirname)
+#define bind_textdomain_codeset(Domainname,Codeset)
+#define textdomain(Domainname)
+#endif
+
 
 /* Global stuff */
 #define PROGRAM_VERSION_STRING PACKAGE_NAME " " VERSION

src/combo.c
@@ -19,8 +19,6 @@
  */
 
 
-#include <config.h>
-
 #include "combo.h"
 
 G_DEFINE_TYPE(Combo,combo,GTK_TYPE_COMBO_BOX_TEXT)

src/configdialog.c
@@ -19,21 +19,17 @@
  */
 
 
-#include <config.h>
-#include <gdk/gdkkeysyms.h>
-
+#include "main.h"
 #include <math.h>
 
 #include "sound.h"
 #include "inifile.h"
-#include "main.h"
 #include "configdialog.h"
 #include "um.h"
 #include "mainwindow.h"
 #include "tempfile.h"
 #include "player.h"
 #include "rateconv.h"
-#include "gettext.h"
 #include "filetypes.h"
 
 G_DEFINE_TYPE (ConfigDialog, config_dialog, GTK_TYPE_DIALOG)

src/dataformat.c
@@ -30,7 +30,6 @@
 #include "dataformat.h"
 #include "inifile.h"
 #include "main.h"
-#include "gettext.h"
 
 Dataformat dataformat_sample_t,dataformat_single;
 gboolean ieee_le_compatible,ieee_be_compatible;

commit ad89195d806268b454fa8c5a09783953bcc9682b

fix application
was broken by a8a2a668989918cd2b204c2217529bfcd83237cd

src/chunk.c
@@ -20,9 +20,8 @@
  */
 
 
-#include <stdio.h>
+#include "main.h"
 #include <unistd.h>
-#include <string.h>
 #include <errno.h>
 #include <math.h>
 #include <sys/stat.h>
@@ -30,7 +29,6 @@
 #include "um.h"
 #include "chunk.h"
 #include "inifile.h"
-#include "main.h"
 #include "ringbuf.h"
 #include "rawdialog.h"
 #include "tempfile.h"

src/chunkview.c
@@ -18,10 +18,9 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
-
+#include "main.h"
 #include <math.h>
 #include "chunkview.h"
-#include "main.h"
 #include "inifile.h"
 #include "player.h"
 #include "mainloop.h"

src/datasource.c
@@ -18,8 +18,7 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
-
-#include <config.h>
+#include "main.h"
 
 #include <sys/stat.h>
 #include <unistd.h>
@@ -27,7 +26,6 @@
 #include "gtkfiles.h"
 #include "ringbuf.h"
 #include "um.h"
-#include "main.h"
 #include "inifile.h"
 
 #include "datasource.h"

src/document.c
@@ -20,7 +20,6 @@
 
 #include <config.h>
 
-
 #include "um.h"
 #include "document.h"
 #include "filetypes.h"

src/main.h
@@ -26,6 +26,8 @@
 
 #include <config.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/time.h>
 #include <sys/types.h>
 #include <time.h>